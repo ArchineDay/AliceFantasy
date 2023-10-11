37c3c456575716ea8b84ef00226cf3f90061f0ae
Archine <2950715596@qq.com>
2023-10-11 23:58:07 +0800

    地图场景搭建

--- Assets/Script/AliceController.cs
+++ Assets/Script/AliceController.cs
@@ -7,12 +7,13 @@ public class AliceController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Application.targetFrameRate= 60;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //update每秒调用60次
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
         
